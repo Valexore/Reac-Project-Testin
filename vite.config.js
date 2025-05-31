c0666b1394e91af0c65d33a895007e332136bd6e
Jira <166265771+Valexore@users.noreply.github.com>
2025-05-31 10:14:32 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Reac-Project-Testin", // Set the base path for your application
+    base: process.env.VITE_BASE_PATH || "/Reac-Project-Testin/", // Set the base path for your application
+
 })
